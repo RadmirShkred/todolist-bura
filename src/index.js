9a06d08a1e36ff07213a8dedee68099e927a2b03
radmir-shkred <psitecton@gmail.com>
2020-05-27 00:48:04 +0300

    commit 3

--- src/index.js
+++ src/index.js
@@ -1,42 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Todolist = () => {
-    const items = ['Learn React', 'Build Awesome App'];
-    return (
-        <ul>
-            <li>{items[0]}</li>
-            <li>{items[1]}</li>
-        </ul>
-    );
-};
-
-const AppHeader = () => {
-    return (
-        <h1>My Todo List</h1>
-    );
-}
-
-const SearchPanel = () => {
-    const searchText = 'Type here to search';
-    const searchStyle = {
-        fontSize: '20px',
-    }
-    return (
-        <input placeholder={searchText} style={searchStyle}/>
-    );
-}
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
 
 const App = () => {
-    const isLoggedIn = true;
-    const loginBox = <span>Log in please</span>;
-    const welcomeBox = <span>Welcome Back</span>;
     return (
         <>
-            <span>{isLoggedIn ? welcomeBox : loginBox}</span>
             <AppHeader/>
             <SearchPanel/>
-            <Todolist/>
+            <TodoList/>
         </>
     );
 }
