1b713ed857c0b427b1244e90851439b6c5dccc4f
radmir-shkred <psitecton@gmail.com>
2020-05-28 00:17:33 +0300

    commit 9

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,6 +3,7 @@ import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
 import ItemStatusFilter from "../item-status-filter";
 import TodoList from "../todo-list";
+import ItemAddForm from "../item-add-form";
 import './app.css';
 
 class App extends Component {
@@ -29,17 +30,16 @@ class App extends Component {
 
     render() {
         return (
-            <div>
-                <div className="todo-app">
-                    <AppHeader toDo={1} done={3}/>
-                    <div className="top-panel d-flex">
-                        <SearchPanel/>
-                        <ItemStatusFilter/>
-                    </div>
-                    <TodoList todos={this.state.todoData}
-                              onDeleted={this.deleteItem}
-                    />
+            <div className="todo-app">
+                <AppHeader toDo={1} done={3}/>
+                <div className="top-panel d-flex">
+                    <SearchPanel/>
+                    <ItemStatusFilter/>
                 </div>
+                <TodoList todos={this.state.todoData}
+                          onDeleted={this.deleteItem}
+                />
+                <ItemAddForm/>
             </div>
         );
     }
