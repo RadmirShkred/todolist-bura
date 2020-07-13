20fe03c3af2151b06452601b10881189f9758d60
radmir-shkred <psitecon@gmail.com>
2020-07-13 20:06:31 +0300

    Updates

---
+++ precache-manifest.4c1f02622022d3ff48b11d2ba1e78d1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d7849499378b105515cb932939b4b9",
+    "url": "/todolist-bura/index.html"
+  },
+  {
+    "revision": "5282d8a1a858425b787d",
+    "url": "/todolist-bura/static/css/main.604a61f3.chunk.css"
+  },
+  {
+    "revision": "ccbb959108f6dff3a820",
+    "url": "/todolist-bura/static/js/2.95328e1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-bura/static/js/2.95328e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5282d8a1a858425b787d",
+    "url": "/todolist-bura/static/js/main.c3161b64.chunk.js"
+  },
+  {
+    "revision": "667e3ff9a310ced0cf0d",
+    "url": "/todolist-bura/static/js/runtime-main.18803d68.js"
+  }
+]);
